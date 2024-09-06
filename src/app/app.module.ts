b7bf1a25855c879c2b42b1973dda803863d2ac8e
Deivison Nogueira <tp.deivison.nogueira@totvspartners.com.br>
2024-09-06 20:50:39 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CidadesComponent } from './components/cidades/cidades.component';
+import { EstadosComponent } from './components/estados/estados.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CidadesComponent,
+    EstadosComponent,
+    LoginComponent,
+    HomeComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
